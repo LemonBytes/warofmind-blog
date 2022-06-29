9edf87b9c5e922ee25bc24368b1b6d9bb16b5800
Wyzbits <wyzbits@gmail.com>
2022-06-29 22:00:40 +0200

    commit after clear cache

---
+++ components/navigation/largeDevice/DesktopNavigation.translation.ts
@@ -0,0 +1,10 @@
+import i18next from 'i18next';
+i18next.addResourceBundle('en', 'desktopNavi', {
+    legalNotice: 'LEGAL NOTICE'
+    
+
+});
+i18next.addResourceBundle('en', 'desktopNavi', {
+    legalNotice: 'IMPRESSUM'
+    
+});
