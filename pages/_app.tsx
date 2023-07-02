d0f59412713e3767924350640cc592b48e626acd
Wyzbits <wyzbits@gmail.com>
2023-07-02 21:03:03 +0200

    added robots.txt

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,7 +19,7 @@ const WarOfMindApp = ({ Component, pageProps }: AppProps) => {
           name="description"
           content="Blog about Martial Arts and building the mind - Embark on a transformative journey through the realms of martial arts and mental health. Join me as I explore the powerful intersection of mind, body, and spirit, sharing insights, techniques, and resources to enhance your well-being. Discover the profound impact of martial arts on mental resilience and personal growth. Uncover the secrets of mindfulness, stress reduction, and emotional well-being. Together, let's unlock the potential within, empowering ourselves to live a harmonious and fulfilling life."
         ></meta>
-        <meta name="robots" content="all"></meta>
+        <meta name="" content="all"></meta>
         <title>War of Mind</title>
         <link rel="icon" href="../public/static/assets/images/favicon.ico" />
       </Head>
